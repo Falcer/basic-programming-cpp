03385b88b5e4a25f551579b7f4335d0be9ce8fd8
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-10-23 12:52:19 +0700

    added basic loop and updated it

--- Loop/basicLoop.cpp
+++ Loop/basicLoop.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 
 int main(){
+	int a[5] = {0,1,2,3,4};
+	int b[5];
+	int len; 
 	
+	int arrlen = sizeof(a) / sizeof(a[0]);
+
+	//Dynamic Array
+	cout<<"Masukkan panjang dari Array C: ";
+	cin>>len;
+
+	int c[len];
+	
+	//Dynamic Value
+	for(int i = 0; i < arrlen;i++){
+		cout<<"Masukkan value dari array b["<<i<<"]: ";
+		cin>>b[i];
+	}
+
+	//Dynamic value and dynamic length of array
+	for(int k = 0; k < len; k++){
+		cout<<"Masukkan value dari array c["<<k<<"]: ";
+		cin>>c[k];
+	}
+
+	//For showing purpose, just change the variable
+	for(int a = 0; a < len; a++){
+		cout<<"Value dari Array ke- "<<a<<": "<<c[a]<<endl;
+	}
 	return 0;
 }
