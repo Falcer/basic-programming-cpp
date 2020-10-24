83681d462b997ed46555b8eeab517e74699bb959
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-10-24 16:09:04 +0700

    update basicFunction(100%)

--- Functions/basicFunction.cpp
+++ Functions/basicFunction.cpp
@@ -15,18 +15,15 @@ int numbers();
 int a = 255; //This one is global variable. Declared outside the main, printOut, numbers scope so the variable can be used in any scope.
 
 //Function with parameters
-double calculate(double operand1, double operand2, char operator);
+double calculate(double operand1, double operand2, char ops);
 void menu();
 void showString(string text1, string text2, string text3);
 
 int main(){
     int b = 88;
-    string line = printOut();
-    int number = numbers();
 
-    cout<<line<<endl<<number;
-    
-    // cout<<endl<<typeid(number).name()<<endl;
+    menu();
+
     return 0;
 }
 
@@ -35,6 +32,57 @@ int numbers() {
     return a;
 }
 
-double calculate() {
+void menu(){
+    int choice;
 
+    cout<<"Welcome to basic function with C++\nPlease select the input you desire: "<<endl;
+    cout<<"1.Basic Function print out string\n2. Basic function print out numbers\n3. Function with 3 String Parameters\n4. Calculator Function\nYour Choice: ";
+    cin>>choice;
+
+    if(choice == 1){
+        cout<<printOut();
+    } else if(choice == 2){
+        cout<<numbers();
+    } else if(choice == 3){
+        showString("Hello", "World", "Test123");
+    } else if(choice == 4){
+        double a,b,c;
+        char op;
+
+        cout<<"Masukkan angka pertama: ";
+        cin>>a;
+        cout<<endl<<"Masukkan angka kedua: ";
+        cin>>b;
+        cout<<endl<<"Masukkan operator: ";
+        cin>>op;
+        c = calculate(a, b, op);
+
+        cout<<"Hasil dari "<<a<<" "<<op<<" "<<b<<" = "<<c<<endl;
+    } else {
+        cout<<"Error!";
+    }
+}
+
+void showString(string text1, string text2, string text3){
+    cout<<"Your first word is " <<text1<<endl;
+    cout<<"Your second word is "<<text2<<endl;
+    cout<<"Your third word is "<<text3<<endl;
+}
+
+double calculate(double operand1, double operand2, char ops) {
+    double hasil;
+
+    if(ops == '+'){
+        hasil = operand1 + operand2;
+    } else if(ops == '-') {
+        hasil = operand1 - operand2;
+    } else if(ops == '*'){
+        hasil = operand1 * operand2;
+    } else if(ops == '/'){
+        hasil = operand1 / operand2;
+    } else {
+        cout<<"Error!";
+    }
+
+    return hasil;
 }
